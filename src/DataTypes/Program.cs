a2e447dca91ef3b9078ff718c568a86ac93da68c
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-12 10:12:34 -0300

    Variables of different data types

---
+++ src/DataTypes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringVar = "Hello guys ";
+            int  intVar = 55;
+            float floatVar = 10.2f;
+            char charVar = ' ';
+            bool boolVar = true;
+
+            var merge = stringVar + charVar + intVar + charVar
+             + floatVar + charVar + boolVar;
+
+            Console.WriteLine(merge);
+        }
+    }
+}
