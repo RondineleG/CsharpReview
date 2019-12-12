295654d22aec99f9113384d4287185e5f1379a1a
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-12 11:36:35 -0300

    GetType of  anonymous type

--- src/AnonymousType/Program.cs
+++ src/AnonymousType/Program.cs
@@ -6,13 +6,16 @@ namespace AnonymousType
     {
         static void Main(string[] args)
         {
-            var myAnonymousType = new {
+            var myAnonymousType = new
+            {
                 fisrtProperty = "First",
                 secondProperty = 2,
                 thirdProperty = true
             };
 
             Console.WriteLine(myAnonymousType.fisrtProperty);
+
+            Console.WriteLine(myAnonymousType.GetType().ToString());
         }
     }
 }
