b82dd659a34feee7cb60223c985f33b42d22229b
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-12 13:28:55 -0300

    Calculate price product sales

---
+++ src/ProductSales/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ProductSales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantity;
+            double price, discount, discountPercent, finalPrice;
+            string name;
+            Console.WriteLine("How name of product ?");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Enter quantity of product");
+            quantity = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter price product");
+            price = double.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Enter discount percent");
+            discountPercent = double.Parse(Console.ReadLine());
+
+
+              finalPrice = quantity * price;
+
+             var finalPriceDiscount = finalPrice - (discountPercent / 100) * finalPrice;
+
+            Console.WriteLine("Result :");
+
+            Console.WriteLine($"Cost Price product is : {finalPriceDiscount}");
+            Console.ReadKey();
+        }
+    }
+}
