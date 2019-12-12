bd35e7a37a52a42b4afade99200099a65ff9bb39
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-12 09:43:01 -0300

    Variable initialization

--- src/Variables/Program.cs
+++ src/Variables/Program.cs
@@ -17,12 +17,32 @@ namespace Variables
              string response = "Fine, thanks";
 
              int i, j, k, l = 0;
+                i =10;
+                j = 5;
+                k = 6;
+                l= i + j + k;
 
              int amount, num;
+
+             amount = i * j;
+             num = amount * l;
+
+             int n, m,
+                p, 
+                h =10; 
+
+                n = 50; m =5; p = 99; 
+                var sum = h *(p- m) + n / 2;
 
             Console.WriteLine("Hello " + friend + message);
 
             Console.WriteLine(response);
+
+            Console.WriteLine(amount);
+
+            Console.WriteLine(num);
+
+            Console.WriteLine(sum);
         }
     }
 }
