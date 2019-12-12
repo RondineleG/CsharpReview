83eb48974f2bd191b343b1546944660763e180b1
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-12 10:02:51 -0300

    Implicitly-Typed variables declarations

--- src/ImplicitlyType/Program.cs
+++ src/ImplicitlyType/Program.cs
@@ -12,6 +12,24 @@ namespace ImplicitlyType
             // inplicitly typed
             var j = 100;
             Console.WriteLine($"This as explicitly typr {i} and this is a implicitly type {j}");
+
+            // Implicitly-Typed variables declarations
+            var age = 27;
+            Console.WriteLine("Type of age is {0}", age.GetType().ToString());
+
+            var name = "Rondinele";
+            Console.WriteLine($"Type of {name} is " + name.GetType().ToString());
+
+
+             var price = 889.82d;
+            Console.WriteLine($"Type of {price} is " + price.GetType().ToString());
+
+
+             var live = true;
+            Console.WriteLine("Type of live is {0}  ",live.GetType().ToString());
+
+
+
         }
     }
 }
