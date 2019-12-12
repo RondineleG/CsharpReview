136f5bed4fe2c7f8375c8e07bc6a47677766c80f
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-12 08:13:57 -0300

    Creating class, constructor and methods

---
+++ src/ProjectClass/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System;
+
+namespace ProjectClass
+{
+    class Program
+    {
+        //Creating class using acces modifier public
+        public class MyClass
+        {
+            //field
+            public string myField = string.Empty;
+            // Constructor 
+            public MyClass()
+            {
+                
+            }
+
+            public void MyMethod (int parametrerOne, int parametrerTwo)
+            {
+                Console.WriteLine("First paramenter {0}, second paramentera {1}", parametrerOne, parametrerTwo);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
