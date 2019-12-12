66267dd3b2909e7b32859febb8e1f50add34d6b1
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-12 09:51:48 -0300

    Variable explicitly and  implicitly type

---
+++ src/ImplicitlyType/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ImplicitlyType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // explicitly typed
+            int i = 100;
+
+            // inplicitly typed
+            var j = 100;
+            Console.WriteLine($"This as explicitly typr {i} and this is a implicitly type {j}");
+        }
+    }
+}
