fb456aa2e1b24405feb1a3f3dbad5c612fe5366c
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-12 12:53:44 -0300

    Methods and Properties of Dynamic Type

--- src/DynamicType/Program.cs
+++ src/DynamicType/Program.cs
@@ -1,7 +1,26 @@
-﻿using System;
+﻿using System.ComponentModel;
+using System.Runtime.ConstrainedExecution;
+using System;
 
 namespace DynamicType
 {
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public int StandardID { get; set; }
+
+        public void DisplayStudentDatails()
+        {
+            Console.WriteLine($"Name : {this.Name}");
+            Console.WriteLine($"Age : {this.Age}");
+            Console.WriteLine($"Standard : {this.StandardID}");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
@@ -22,6 +41,9 @@ namespace DynamicType
             dynamicVariable = DateTime.Now;
             Console.WriteLine("Dynamic variable value: {0}, Type: {1}", dynamicVariable, dynamicVariable.GetType().ToString());
 
+            dynamic dynamicStudent = new Student();
+
+            dynamicStudent.DisplayStudentDatails();
 
         }
     }
