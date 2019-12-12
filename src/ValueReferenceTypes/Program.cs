a7358135d4493624bf52580d8103b274a954f34f
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-12 11:08:53 -0300

    Passing by Value

---
+++ src/ValueReferenceTypes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+using System;
+
+namespace ValueReferenceTypes
+{
+    class Program
+    {
+        static void ChangeValue(int x)
+        {
+           x = 500;
+           Console.WriteLine(x);
+        }
+        static void Main(string[] args)
+        {
+            int i = 100;
+
+            Console.WriteLine(i);
+
+            ChangeValue(i);
+
+            Console.WriteLine(i);
+           
+        }
+    }
+}
