d709d3a68806da20400769fcaeca38567e8c7210
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-12 13:40:58 -0300

    Creating Interfaces

---
+++ src/ProjectInterfaces/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ProjectInterfaces
+{
+    interface IPen
+    {
+        string Color { get; set; }
+
+        bool Open();
+
+        bool Close();
+
+        void Write(string text);
+    }
+
+    public class Cello : IPen   
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
