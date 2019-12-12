54bbf8b194acb2f93933a27f575bbcedc14e75a6
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-12 12:03:26 -0300

    Anonymous Types with a LINQ Query

--- src/AnonymousType/Program.cs
+++ src/AnonymousType/Program.cs
@@ -1,7 +1,16 @@
-﻿using System;
-
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
 namespace AnonymousType
 {
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
@@ -23,6 +32,22 @@ namespace AnonymousType
             Console.WriteLine(myAnonymousType.GetType().ToString());
 
             DoSomething(myAnonymousType);
+
+            IList<Student> studentList = new List<Student>()
+            {
+                new Student(){Id =1, Name ="Rondinele", Age=27},
+                new Student(){Id =2, Name ="Leticia", Age=25},
+                new Student(){Id =3, Name ="Ronaldo", Age=43},
+                new Student(){Id =4, Name ="Elen", Age=48},
+                new Student(){Id =5, Name ="Maria", Age=17}
+            };
+
+            var studentNames = from s in studentList
+                               select new
+                               {
+                                   Id = s.Id,
+                                   Name = s.Name
+                               };
 
         }
 
