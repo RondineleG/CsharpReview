c388df2cc8bc2bca59652f4b1b6236018b143014
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-12 09:18:16 -0300

    property with encapsulates a private field

--- src/ProjectClass/Program.cs
+++ src/ProjectClass/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System.Dynamic;
+using System.Security.Cryptography.X509Certificates;
 using System;
 
 namespace ProjectClass
@@ -13,13 +14,33 @@ namespace ProjectClass
             // Constructor 
             public MyClass()
             {
-                
+
             }
 
-            public void MyMethod (int parametrerOne, int parametrerTwo)
+            //Method/Function
+            public void MyMethod(int parametrerOne, int parametrerTwo)
             {
                 Console.WriteLine("First paramenter {0}, second paramentera {1}", parametrerOne, parametrerTwo);
             }
+             
+             // Auto-implemented property
+        public MyClass(int autoProperty) 
+        {
+            this.AutoProperty = autoProperty;
+               
+        }
+                     public int AutoProperty { get; set; }
+
+             private int _myProperty;
+
+             //property
+             public int MyProperty
+             {
+                 get{return _myProperty;}
+                 set{_myProperty = value;}
+             }
+
+
         }
         static void Main(string[] args)
         {
