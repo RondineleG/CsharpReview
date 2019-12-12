2179063f9d8fac41edc9e2afb9b436f5c525892d
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-12 11:31:44 -0300

    Creating anonymous type

---
+++ src/AnonymousType/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AnonymousType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myAnonymousType = new {
+                fisrtProperty = "First",
+                secondProperty = 2,
+                thirdProperty = true
+            };
+
+            Console.WriteLine(myAnonymousType.fisrtProperty);
+        }
+    }
+}
