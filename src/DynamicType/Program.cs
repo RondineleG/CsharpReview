2857ef0b5633d20042e654c3a323d2985ea74042
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-12 12:45:25 -0300

    Creating dynamic types

---
+++ src/DynamicType/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DynamicType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic dynamicVar = "Rondinele";
+
+            Console.WriteLine(dynamicVar.GetType().ToString());
+
+            dynamic dynamicVariable = 100;
+            Console.WriteLine($"Dynamic variable value: {dynamicVariable}, Type: " + dynamicVariable.GetType().ToString());
+
+            dynamicVariable = "Save the World!!";
+            Console.WriteLine($"Dynamic variable value: {dynamicVariable}, Type: " + dynamicVariable.GetType().ToString());
+
+            dynamicVariable = true;
+            Console.WriteLine("Dynamic variable value: {0}, Type: {1}", dynamicVariable, dynamicVariable.GetType().ToString());
+
+            dynamicVariable = DateTime.Now;
+            Console.WriteLine("Dynamic variable value: {0}, Type: {1}", dynamicVariable, dynamicVariable.GetType().ToString());
+
+
+        }
+    }
+}
