8c1c679b1767585398e8c3f35d8d5f19f16e6857
Rondinele Guimaraes <rondineleg@edu.univali.br>
2019-12-12 01:01:12 -0300

    Back to basics

--- src/HelloWorld/Program.cs
+++ src/HelloWorld/Program.cs
@@ -6,7 +6,11 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var name = "Rondinele";
+            Console.WriteLine("Hi " + name);
+            Console.WriteLine("How are you today ?");
+            var mensage = Console.ReadLine();
+            System.Console.WriteLine(mensage);
         }
     }
 }
