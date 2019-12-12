51881295c4529e5bc3faba1108453980030160c1
Rondinele Guimaraes <rondineleg@edu.univali.br>
2019-12-12 01:11:54 -0300

    Creating varibles and i using interpolated

---
+++ src/Variables/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var friend = "Dog";   
+             Console.WriteLine($"Hello {friend}");
+            Console.WriteLine(friend); 
+                friend = "Cat";
+            Console.WriteLine(friend);
+
+            Console.WriteLine("Hello " + friend);
+        }
+    }
+}
