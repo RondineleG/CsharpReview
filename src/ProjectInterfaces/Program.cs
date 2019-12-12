23ebad4e84773a82bc053b0a02ecae2616196bb5
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-12 13:51:35 -0300

    Implementing interfaces

--- src/ProjectInterfaces/Program.cs
+++ src/ProjectInterfaces/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Net.NetworkInformation;
+using System;
 
 namespace ProjectInterfaces
 {
@@ -13,15 +14,47 @@ namespace ProjectInterfaces
         void Write(string text);
     }
 
-    public class Cello : IPen   
+    public class Cello: IPen 
     {
+       public string Color{get; set;}
+
+       private bool isOpen = false;
+
+       public bool Close()
+       {
+           isOpen = false;
+           Console.WriteLine("Cello closed for wrinting");
+
+           return isOpen;
+
+       }
+
+       public bool Open()
+       {
+           isOpen = true;
+           Console.WriteLine("Cello open for writing");
+
+           return isOpen;
+
+       }
+
+       public void Write(string text)
+       {
+            if(isOpen)
+            {
+                Console.WriteLine($"Cello : {text}");
+            }
+       }
 
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+          IPen pen = new Cello();
+          pen.Open();
+          pen.Write("This is may pen .");
+          pen.Close();
         }
     }
 }
