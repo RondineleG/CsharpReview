32bf062ab5e49a665eba85e6ab50e88713cc563e
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-12 14:18:48 -0300

    Interface Type Variable

--- src/ProjectInterfaces/Program.cs
+++ src/ProjectInterfaces/Program.cs
@@ -14,85 +14,131 @@ namespace ProjectInterfaces
         void Write(string text);
     }
 
-    public class Cello: IPen 
+    public class Cello : IPen
     {
-       public string Color{get; set;}
+        public string Color { get; set; }
 
-       private bool isOpen = false;
+        private bool isOpen = false;
 
-       public bool Close()
-       {
-           isOpen = false;
-           Console.WriteLine("Cello closed for wrinting");
+        public bool Close()
+        {
+            isOpen = false;
+            Console.WriteLine("Cello closed for wrinting");
 
-           return isOpen;
+            return isOpen;
 
-       }
+        }
 
-       public bool Open()
-       {
-           isOpen = true;
-           Console.WriteLine("Cello open for writing");
+        public bool Open()
+        {
+            isOpen = true;
+            Console.WriteLine("Cello open for writing");
 
-           return isOpen;
+            return isOpen;
 
-       }
+        }
 
-       public void Write(string text)
-       {
-            if(isOpen)
+        public void Write(string text)
+        {
+            if (isOpen)
             {
                 Console.WriteLine($"Cello : {text}");
             }
-       }
+        }
 
     }
-    
+
     public class Pencil : IPen
-    { 
-        string IPen.Color {get;set;}
+    {
+        string IPen.Color { get; set; }
 
         private bool isOpen = false;
 
         bool IPen.Close()
         {
-             isOpen = false;
-           Console.WriteLine("Pencil closed for wrinting");
+            isOpen = false;
+            Console.WriteLine("Pencil closed for wrinting");
 
-           return isOpen;
+            return isOpen;
 
         }
 
         bool IPen.Open()
         {
             isOpen = true;
-           Console.WriteLine("Pencil open for wrinting");
+            Console.WriteLine("Pencil open for wrinting");
 
-           return isOpen;
+            return isOpen;
         }
 
         void IPen.Write(string text)
         {
-             if(isOpen)
+            if (isOpen)
             {
                 Console.WriteLine($"Pencil : {text}");
             }
         }
 
     }
+    class Parker : IPen
+    {
+        public string Color { get; set; }
+
+        private bool canWrite = false;
+
+        public bool Close()
+        {
+            canWrite = false;
+            Console.WriteLine("Parker is closed ");
+
+            return canWrite;
+        }
+
+        public bool Open()
+        {
+            canWrite = true;
+            Console.WriteLine("Parker is open ");
+
+            return canWrite;
+        }
+
+        public void Write(string text)
+        {
+            //write text if open
+            if (canWrite)
+                Console.WriteLine("Parker: " + text);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-          IPen pen = new Cello();
-          pen.Open();
-          pen.Write("This is may Pen .");
-          pen.Close();
+            IPen pen = new Cello();
+            pen.Open();
+            pen.Write("This is may Pen .");
+            pen.Close();
 
-          IPen pencil = new Pencil();
-           pencil.Open();
-          pencil.Write("This is may pencil .");
-          pencil.Close();
+            IPen pencil = new Pencil();
+            pencil.Open();
+            pencil.Write("This is may pencil .");
+            pencil.Close();
+
+
+            IPen parker = new Parker();
+            parker.Open();
+            parker.Write($"Parker : {parker}");
+            parker.Close();
+
+            //Example o interfaces type variable
+
+            IPen pen1 = new Cello();
+            IPen pen2 = new Parker();
+            IPen pen3 = new Pencil();
+
+
+            pen3 = new Cello();
+            pen1 = new Parker();
+            pen2 = new Pencil();
 
         }
     }
