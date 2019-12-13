192bc2f32a0cb45e83bbf685da882e4da0c99c42
Rondinele Guimaraes <rondineleg@edu.univali.br>
2019-12-13 11:10:25 -0300

    Creating switch

---
+++ src/ConsoleApps/Switch/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 15;
+            switch(x)
+            {
+                case 5:
+                    Console.WriteLine("Value of x is 5");
+                    break;
+                case 10:
+                    Console.WriteLine("Value of x is 10");
+                    break;
+                case 15:
+                    Console.WriteLine("Value of x is 15");
+                    break;
+                default:
+                    Console.WriteLine("Unknown value");
+                    break;
+
+            }
+            
+        }
+    }
+}
