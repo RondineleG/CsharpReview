f74b0fb51e71e2e1d8198905fe45bd4e14211ccd
Rondinele Guimaraes <rondineleg@edu.univali.br>
2019-12-13 10:48:51 -0300

    Ternary operator replaces if statement

--- src/ConsoleApps/TernaryOperator/Program.cs
+++ src/ConsoleApps/TernaryOperator/Program.cs
@@ -7,10 +7,27 @@ namespace TernaryOperator
         static void Main(string[] args)
         {
             int x = 20, y = 10;
+            int result3 = 0;
+
 
             var result = x > y ? "x is greater than y" : "x is less than or iqual to y";
+            var result2 = x > y ? x : y;
+
+            if(x >y )
+            {
+                result3 = x;
+            }
+            else if (x < y)
+            {
+                result3 = y;
+            }
 
             Console.WriteLine(result);
+
+            Console.WriteLine(result2);
+            
+            Console.WriteLine(result3);
+
         }
     }
 }
