c3689fe153e39acb7855c5fcea67a86e56254dd0
Rondinele Guimaraes <rondineleg@edu.univali.br>
2019-12-14 01:39:29 -0300

    for loop

---
+++ src/ConsoleApps/ForLoop/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <10; i++)
+            {
+                Console.WriteLine("Value of i : {0}", i);
+            }
+
+            int loopFor = 0;
+
+            for(; ; )
+            {
+                if(loopFor < 10)
+                {
+                    Console.WriteLine("Value of i : {0}", loopFor);
+                    loopFor++;
+
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
