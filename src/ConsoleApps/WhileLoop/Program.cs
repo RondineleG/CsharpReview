50154d5d97df7658f8e8ad8626777be0637cce89
Rondinele Guimaraes <rondineleg@edu.univali.br>
2019-12-22 01:39:52 -0300

    break in while loop

--- src/ConsoleApps/WhileLoop/Program.cs
+++ src/ConsoleApps/WhileLoop/Program.cs
@@ -14,6 +14,27 @@ namespace c
                 i++;
             }
             Console.WriteLine(i);
+
+            int age = 0;
+
+            while(true)
+            {
+                Console.WriteLine("Value of age is : {0}", age);
+                age++;
+
+                if (age > 18)
+                {
+                    break;
+                }
+                else if (age  == 18)
+                {
+                    Console.WriteLine("it's equal than 18");
+                }
+                else
+                {
+                    Console.WriteLine("it's smaller than 18");
+                }
+            }
         }
     }
 }
